9a86c7d8d9328110cf56c267fbf1ddde819133b0
millenialia <lenalebo@gmail.com>
2023-06-04 21:21:16 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ const errorEl = document.querySelector('.error')
 const loaderEl = document.querySelector('.loader')
 
 errorEl.classList.toggle('is-hidden')
+selectEl.classList.toggle('is-hidden')
 
 selectEl.addEventListener('change', onChange)
 
 fetchBreeds().then(renderCatOptions).catch(() => {
       errorEl.classList.toggle('is-hidden')
-  }).finally(() => loaderEl.classList.toggle('is-hidden'));
+}).finally(() => {
+  loaderEl.classList.toggle('is-hidden')
+  selectEl.classList.toggle('is-hidden')
+});
 
 function onChange(event) {
   loaderEl.classList.toggle('is-hidden')
+  selectEl.classList.toggle('is-hidden')
   catInfo.innerHTML =''
   const breadID = event.target.value
   fetchCatByBreed(breadID)
@@ -22,7 +27,7 @@ function onChange(event) {
     const breedObj = breedArr[0].breeds[0];
     const breedImg = breedArr[0].url
     renderCatCard(breedImg, breedObj)
-    selectEl.classList.remove('is-hidden')
+    selectEl.classList.toggle('is-hidden')
     })
     .catch(() => {
       errorEl.classList.toggle('is-hidden')
@@ -34,7 +39,8 @@ function onChange(event) {
 
 function renderCatOptions(breeds){
     const markup = breeds.map((breed) => `<option value='${breed.id}'>${breed.name}</option>`).join('')
-    selectEl.insertAdjacentHTML("beforeend", markup)
+  selectEl.insertAdjacentHTML("beforeend", markup)
+
 }
 
 function renderCatCard(breedImg, breedObj) {
